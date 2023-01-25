5d2fe69b53c4582b3e6b72dc58fb5bb460b6115f
EvgenijKobzar <9097963939@rambler.ru>
2023-01-25 17:50:33 +0200

    include route

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
+import router from './router'
 import './style.css'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
 library.add(faHatWizard)
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
