76748d674ffb91e5540e81a7be13b08c975d15c9
EvgenijKobzar <9097963939@rambler.ru>
2023-01-26 18:03:23 +0200

    добавлен http-client

--- src/main.js
+++ src/main.js
@@ -7,4 +7,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 library.add(faPlus)
 
+import httpClient from "./http-client/index.js";
+httpClient.get( 'http://m.ft24.ru/rest/mft.preset.getFields.json', null, function(xhr) {
+	//console.log( JSON.parse( xhr.responseText ) );
+	//response = JSON.parse( xhr.responseText );
+
+	//console.log( list, gists );
+}).then((result) => console.log( result));
+
 createApp(App).use(router).mount('#app')
