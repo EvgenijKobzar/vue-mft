14be2bb64f8cf089c3eb79a402d2999dc2fd09be
EvgenijKobzar <9097963939@rambler.ru>
2023-02-11 15:59:18 +0200

    добавил график vue-chartjs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-// import { ref, onMounted, computed } from 'vue'
+import {ref, onMounted, computed, reactive} from 'vue'
 // import ReportCollection from "./lib/collection/report-collection.js";
 
 // const items = ref([]);
@@ -33,6 +33,106 @@
 // {
 // 	return '/' + item + '/' + type;
 // }
+import {
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	BarElement,
+	CategoryScale,
+	LinearScale
+} from 'chart.js'
+import { Bar, Line } from 'vue-chartjs'
+import Rest from "./lib/provider/rest.js";
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+const state = reactive({
+	status: 'wait',
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [
+			{
+				label: 'Расход л/100км',
+				backgroundColor: '#77b7cd',
+				data: []
+			}
+		],
+	},
+	options: {
+		responsive: true,
+		aspectRatio: 1,
+		maintainAspectRatio: false,
+		plugins: {
+			legend: {
+				display: true,
+				labels: {
+					color: 'rgb(255, 99, 132)'
+				}
+			}
+		}
+	},
+	data1: {
+		labels: [],
+		datasets: [
+			{
+				label: 'Расход л/моточас',
+				backgroundColor: '#77b7cd',
+				data: []
+			}
+		],
+	},
+	options1: {
+		responsive: true,
+		aspectRatio: 1,
+		maintainAspectRatio: false,
+		plugins: {
+			legend: {
+				display: true,
+				labels: {
+					color: 'rgb(255, 99, 132)'
+				}
+			}
+		}
+	}
+})
+
+const labels = []
+const datasets = []
+const labels1 = []
+const datasets1 = []
+
+onMounted(()=> {
+	return new Promise((resolve, reject) =>
+	{
+		(new Rest({cmd: 'mft.transportation.list'}))
+		.then((response) =>
+		{
+			response.data.result.forEach((item, index) =>
+			{
+				if (item['status'] === 'completed')
+				{
+					labels.push(item['dateTo']);
+					labels1.push(item['dateTo']);
+					datasets.push(parseFloat(item['vehicleFuelConsumption']));
+					datasets1.push(parseFloat(item['trailerFuelConsumption']));
+				}
+			})
+
+			state.data.labels = labels;
+			state.data1.labels = labels1;
+			state.data.datasets[0].data = datasets
+			state.data1.datasets[0].data = datasets1
+
+			console.log('state', state)
+			state.status = 'none';
+			resolve();
+		})
+	})
+})
+
+
+
 
 </script>
 
@@ -43,6 +143,13 @@
 <!--			<router-link :to="makeUrl(item, type)" v-for="(type, i) in types" > {{item}} {{type}} | </router-link> |-->
 <!--		</div>-->
 <!--	</div>-->
+
+<!--	<div class="chart-container" style="position: relative; height:40vh; width:80vw" ><Bar :data="state.data" :options="state.options" v-if="state.status === 'none'"/></div>-->
+<!--	<div class="chart-container" style="position: relative; height:40vh; width:80vw"><Bar :data="state.data1" :options="state.options1" v-if="state.status === 'none'"/></div>-->
+
+	<div><Bar :data="state.data" :options="state.options" v-if="state.status === 'none'"/></div>
+	<div><Bar :data="state.data1" :options="state.options1" v-if="state.status === 'none'"/></div>
+
 	<router-view />
 	<router-view name="navbar" />
 </template>
